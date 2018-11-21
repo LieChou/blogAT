efa33ffcb171c5408bd3f4671bf104bbc20ba4e0
AurelieT <aurelie.tudare@gmail.com>
2018-11-21 21:44:08 +0100

    ajout infos pour classe database

--- core/database.php
+++ core/database.php
@@ -11,5 +11,8 @@ class Manager
     }
 }
 
+//class Manager {private $db_host;private $db_name;private $db_user;private $db_password;
+// public function __construct($db_host='localhost', $db_name, $db_user='', $db_password=''){$this->db_host=$db_host;$this->db_name=$db_name;$this->db_user=$db_user;$this->db_password=$db_password;}
+
 
 
